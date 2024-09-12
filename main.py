1a18a013019e4c82a00e530dce6eec0aa52cb1d6
Siddarth-42110 <sid.sivaraman@gmail.com>
2024-09-12 17:26:20 +0100

    intersection of set

---
+++ main.py
@@ -0,0 +1,9 @@
+set1 = {'A', 'B', 'C', 'D', 'E'}
+set2 = {'B', 'D', 'V', 'X', 'Y', 'Z'}
+
+intersection = set1.intersection(set2)
+
+total_guests = list(intersection)
+
+print("Total guests to be invited in the party are:", len(total_guests))
+print("Guest list:", total_guests)
